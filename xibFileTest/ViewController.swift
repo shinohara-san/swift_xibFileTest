0c0692f86d68e3b19ca8cc05eebc121beed0850f
shinohara-san <someiyoshino0809@gmail.com>
2020-06-02 14:40:52 +0900

    Initial Commit

---
+++ xibFileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xibFileTest
+//
+//  Created by Yuki Shinohara on 2020/06/02.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
